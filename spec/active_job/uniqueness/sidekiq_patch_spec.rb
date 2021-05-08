bae406db36137a69da694457d7d0aad4fcae689a
Rustam Sharshenov <rustam@sharshenov.com>
2021-05-08 20:29:16 +0100

    Use rspec tags for Sidekiq specs

--- spec/active_job/uniqueness/sidekiq_patch_spec.rb
+++ spec/active_job/uniqueness/sidekiq_patch_spec.rb
@@ -1,141 +1,140 @@
 # frozen_string_literal: true
 
-if defined?(Sidekiq)
-  describe 'Sidekiq patch', type: :integration do
-    shared_examples_for 'locks release' do
-      class self::SidekiqWorker
-        include Sidekiq::Worker
-      end
+describe 'Sidekiq patch', :sidekiq, type: :integration do
+  shared_examples_for 'locks release' do
+    let(:sidekiq_worker) do
+      stub_const('MySidekiqWorker', Class.new { include Sidekiq::Worker })
+    end
 
-      context 'when queue adapter is Sidekiq', active_job_adapter: :sidekiq do
-        context 'when job class has unique strategy enabled' do
-          class self::Job < ActiveJob::Base
-            unique :until_executed
-          end
+    after { Sidekiq::Queue.all.each(&:clear) }
 
-          it 'releases the lock' do
-            expect { subject }.to change { locks(job_class_name: self.class::Job.name).count }.by(-1)
-          end
+    context 'when queue adapter is Sidekiq', active_job_adapter: :sidekiq do
+      context 'when job class has unique strategy enabled' do
+        let!(:activejob_worker) do
+          stub_const('MyActiveJob', Class.new(ActiveJob::Base) { unique :until_executed })
         end
 
-        context 'when job class has no unique strategy enabled' do
-          class self::Job < ActiveJob::Base
-          end
-
-          include_examples 'no unlock attempts'
+        it 'releases the lock' do
+          expect { subject }.to change { locks(job_class_name: activejob_worker.name).count }.by(-1)
         end
       end
 
-      context 'when queue adapter is not Sidekiq', active_job_adapter: :test do
-        context 'when job class has unique strategy enabled' do
-          class self::Job < ActiveJob::Base
-            unique :until_executed
-          end
-
-          include_examples 'no unlock attempts'
-        end
-      end
-    end
-
-    describe 'scheduled set item delete' do
-      subject { Sidekiq::ScheduledSet.new.each(&:delete) }
-
-      before do
-        Sidekiq::ScheduledSet.new.clear
-        self.class::SidekiqWorker.perform_in(3.minutes, 123)
-        self.class::Job.set(wait: 3.minutes).perform_later(321)
-      end
-
-      include_examples 'locks release'
-    end
-
-    describe 'scheduled set item remove_job' do
-      subject { Sidekiq::ScheduledSet.new.each { |entry| entry.send(:remove_job) { |_| } } }
-
-      before do
-        Sidekiq::ScheduledSet.new.clear
-        self.class::SidekiqWorker.perform_in(3.minutes, 123)
-        self.class::Job.set(wait: 3.minutes).perform_later(321)
-      end
-
-      include_examples 'locks release'
-    end
-
-    describe 'scheduled set clear' do
-      subject { Sidekiq::ScheduledSet.new.clear }
-
-      before do
-        Sidekiq::ScheduledSet.new.clear
-        self.class::SidekiqWorker.perform_in(3.minutes, 123)
-        self.class::Job.set(wait: 3.minutes).perform_later(321)
-      end
-
-      include_examples 'locks release'
-    end
-
-    describe 'job delete' do
-      subject { Sidekiq::Queue.new('default').each(&:delete) }
-
-      before do
-        Sidekiq::Queue.new('default').clear
-        self.class::SidekiqWorker.perform_async(123)
-        self.class::Job.perform_later(321)
-      end
-
-      include_examples 'locks release'
-    end
-
-    describe 'queue clear' do
-      subject { Sidekiq::Queue.new('default').clear }
-
-      before do
-        Sidekiq::Queue.new('default').clear
-        self.class::SidekiqWorker.perform_async(123)
-        self.class::Job.perform_later(321)
-      end
-
-      include_examples 'locks release'
-    end
-
-    describe 'job set clear' do
-      subject { Sidekiq::JobSet.new('schedule').clear }
-
-      before do
-        Sidekiq::JobSet.new('schedule').clear
-        self.class::SidekiqWorker.perform_in(3.minutes, 123)
-        self.class::Job.set(wait: 3.minutes).perform_later(321)
-      end
-
-      include_examples 'locks release'
-    end
-
-    describe 'job delete' do
-      subject { Sidekiq::Queue.new('default').each(&:delete) }
-
-      before do
-        self.class::SidekiqWorker.perform_async(123)
-        self.class::Job.perform_later(321)
-      end
-
-      include_examples 'locks release'
-    end
-
-    if Gem::Version.new(Sidekiq::VERSION) >= Gem::Version.new('5.1')
-      describe 'job death' do
-        subject do
-          Sidekiq::Queue.new('default').each do |job|
-            Sidekiq::DeadSet.new.kill(job.value)
-          end
+      context 'when job class has no unique strategy enabled' do
+        let!(:activejob_worker) do
+          stub_const('MyActiveJob', Class.new(ActiveJob::Base))
         end
 
-        before do
-          Sidekiq::Queue.new('default').clear
-          self.class::SidekiqWorker.perform_async(123)
-          self.class::Job.perform_later(321)
-        end
-
-        include_examples 'locks release'
+        include_examples 'no unlock attempts'
       end
     end
+
+    context 'when queue adapter is not Sidekiq', active_job_adapter: :test do
+      context 'when job class has unique strategy enabled' do
+        let!(:activejob_worker) do
+          stub_const('MyActiveJob', Class.new(ActiveJob::Base) { unique :until_executed })
+        end
+
+        include_examples 'no unlock attempts'
+      end
+    end
+  end
+
+  describe 'scheduled set item delete' do
+    subject { Sidekiq::ScheduledSet.new.each(&:delete) }
+
+    before do
+      Sidekiq::ScheduledSet.new.clear
+      sidekiq_worker.perform_in(3.minutes, 123)
+      activejob_worker.set(wait: 3.minutes).perform_later(321)
+    end
+
+    include_examples 'locks release'
+  end
+
+  describe 'scheduled set item remove_job' do
+    subject { Sidekiq::ScheduledSet.new.each { |entry| entry.send(:remove_job) { |_| } } }
+
+    before do
+      Sidekiq::ScheduledSet.new.clear
+      sidekiq_worker.perform_in(3.minutes, 123)
+      activejob_worker.set(wait: 3.minutes).perform_later(321)
+    end
+
+    include_examples 'locks release'
+  end
+
+  describe 'scheduled set clear' do
+    subject { Sidekiq::ScheduledSet.new.clear }
+
+    before do
+      Sidekiq::ScheduledSet.new.clear
+      sidekiq_worker.perform_in(3.minutes, 123)
+      activejob_worker.set(wait: 3.minutes).perform_later(321)
+    end
+
+    include_examples 'locks release'
+  end
+
+  describe 'job delete' do
+    subject { Sidekiq::Queue.new('default').each(&:delete) }
+
+    before do
+      Sidekiq::Queue.new('default').clear
+      sidekiq_worker.perform_async(123)
+      activejob_worker.perform_later(321)
+    end
+
+    include_examples 'locks release'
+  end
+
+  describe 'queue clear' do
+    subject { Sidekiq::Queue.new('default').clear }
+
+    before do
+      Sidekiq::Queue.new('default').clear
+      sidekiq_worker.perform_async(123)
+      activejob_worker.perform_later(321)
+    end
+
+    include_examples 'locks release'
+  end
+
+  describe 'job set clear' do
+    subject { Sidekiq::JobSet.new('schedule').clear }
+
+    before do
+      Sidekiq::JobSet.new('schedule').clear
+      sidekiq_worker.perform_in(3.minutes, 123)
+      activejob_worker.set(wait: 3.minutes).perform_later(321)
+    end
+
+    include_examples 'locks release'
+  end
+
+  describe 'job delete' do
+    subject { Sidekiq::Queue.new('default').each(&:delete) }
+
+    before do
+      sidekiq_worker.perform_async(123)
+      activejob_worker.perform_later(321)
+    end
+
+    include_examples 'locks release'
+  end
+
+  describe 'job death', sidekiq: :job_death do
+    subject do
+      Sidekiq::Queue.new('default').each do |job|
+        Sidekiq::DeadSet.new.kill(job.value)
+      end
+    end
+
+    before do
+      Sidekiq::Queue.new('default').clear
+      sidekiq_worker.perform_async(123)
+      activejob_worker.perform_later(321)
+    end
+
+    include_examples 'locks release'
   end
 end
